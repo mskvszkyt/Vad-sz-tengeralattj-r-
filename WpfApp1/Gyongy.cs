1d632bddf9c9607cbbb31deb4cedf05791f7d15a
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2024-03-20 12:57:05 +0100

    Add files via upload

---
+++ WpfApp1/Gyongy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Gyongy
+    {
+        int _x, _y, _z, _e;
+
+        public Gyongy(int x, int y, int z, int e)
+        {
+            this._x = x;
+            this._y = y;
+            this._z = z;
+            this._e = e;
+        }
+
+        public Gyongy(string CSVsor)
+        {
+            var sor = CSVsor.Trim().Split(';');
+            _x = int.Parse(sor[0]);
+            _y = int.Parse(sor[1]);
+            _z = int.Parse(sor[2]);
+            _e = int.Parse(sor[3]);
+        }
+
+        public int X { get => _x; }
+        public int Y { get => _y; }
+        public int Z { get => _z; }
+        public int E { get => _e; }
+
+        public static double Distance(Gyongy first, Gyongy second)
+        {
+            return Math.Sqrt(Math.Pow(second.X - first.X,2) + Math.Pow(second.Y - first.Y, 2) + Math.Pow(second.Z - first.Z, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"{X};{Y};{Z};{E}";
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using HelixToolkit.Wpf;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gyongy> gyongyok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            File.ReadAllLines("gyongyok.txt").Skip(1).ToList().ForEach(line => gyongyok.Add(new Gyongy(line)));
+            foreach (Gyongy gyongy in gyongyok)
+            {
+                EllipsoidVisual3D gyongy3D = new();
+                gyongy3D.RadiusX = gyongy3D.RadiusY = gyongy3D.RadiusZ = gyongy.E;
+                gyongy3D.Center = new System.Windows.Media.Media3D.Point3D(gyongy.X * 3.5, gyongy.Y * 3.5, gyongy.Z * 3.5);
+                gyongy3D.Fill = new SolidColorBrush(Colors.Black);
+                ter.Children.Add(gyongy3D);
+            }
+        }
+
+        //public List<Gyongy> UtvonalKereses()
+        //{
+        //    return gyongyok;
+        //}
+    }
+}
